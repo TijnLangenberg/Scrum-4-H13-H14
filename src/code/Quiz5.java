35b2ee2f9ed20c144a5df63f581ffe4f465d6a1f
tijn <tijn@10.150.140.141>
2019-06-19 10:33:02 +0200

    .

---
+++ src/code/Quiz5.java
@@ -0,0 +1,9 @@
+package code;
+
+public abstract class Quiz5 {
+	
+	int x;
+	abstract void methodeA();
+	abstract String methodeB(String s);
+
+}
---
+++ src/code/Quiz5deel2.java
@@ -0,0 +1,31 @@
+package code;
+
+public class Quiz5deel2 extends Quiz5 {
+	
+	void methodeA() {
+		
+		System.out.print("x");
+	}
+	void methodeA(int x) {
+		
+		System.out.print("y" + "x");
+	}
+	String methodeB(String s) {
+		
+		return s + x;
+	}
+	String methodeB(int x) {
+		
+		return "x" + x;
+	}
+	public static void main(String[] args) {
+		
+		Quiz5deel2 mk = new Quiz5deel2();
+		mk.Quiz5deel2();
+		System.out.print(mk.methodeB("y"));
+	}
+	private void Quiz5deel2() {
+		// TODO Auto-generated method stub
+		
+	}
+}
