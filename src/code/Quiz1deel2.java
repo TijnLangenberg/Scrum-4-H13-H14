8fe974b4e92575beae888d3b2da3b6cab90f4857
tijn <tijn@10.150.140.141>
2019-06-19 10:02:26 +0200

    Quiz 1

--- src/code/Quiz1deel2.java
+++ src/code/Quiz1deel2.java
@@ -1,5 +1,18 @@
 package code;
 
-public class Quiz1deel2 {
-
+public class Quiz1deel2 extends Quiz1 {
+	
+	Quiz1deel2() {
+		this(2);
+		str += 7;
+	}
+	Quiz1deel2(int x) {
+		str += x;
+	}
+	
+	public static void main(String[] args) {
+		
+		Quiz1deel2 mk = new Quiz1deel2();
+		System.out.print(mk.str);
+	}
 }
