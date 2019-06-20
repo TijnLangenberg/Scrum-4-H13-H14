c21090d841bb1205044563c58eeae24e0d8c767b
tijn <tijn@10.150.140.157>
2019-06-20 13:11:35 +0200

    .

---
+++ src/H13/h13quiz2/Quiz2.java
@@ -0,0 +1,10 @@
+package src.H13.h13quiz2;
+
+public abstract class Quiz2 {
+	
+	Quiz2() {
+		
+		System.out.print("n");
+	}
+	abstract void printletter(char c);
+}
---
+++ src/H13/h13quiz2/Quiz2deel3.java
@@ -0,0 +1,13 @@
+package src.H13.h13quiz2;
+
+public class Quiz2deel3 extends Quiz2deel2 {
+	
+	Quiz2deel3() {
+		
+		System.out.print("p");
+	}
+	public static void main (String[] args) {
+		
+		new Quiz2deel3().printletter('s');
+	}
+}
