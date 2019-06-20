b818ce48a93e2b22e176789a35256a5e7d69f110
blacksoijo2 <47447116+blacksoijo2@users.noreply.github.com>
2019-06-20 13:29:32 +0200

    l

---
+++ src/H14/h14quiz2/InterfaceB.java
@@ -0,0 +1,7 @@
+package _14_interfaces.qz02;
+
+public interface InterfaceB
+{
+  int MAX_TOEGESTAAN = 3;
+  int som();
+}
---
+++ src/H14/h14quiz2/MijnKlasse.java
@@ -0,0 +1,28 @@
+package _14_interfaces.qz02;
+
+public class MijnKlasse implements InterfaceA,InterfaceB
+{
+  // int MAX_TOEGESTAAN; /*1*/
+  int z = 2;
+  public void mijnMethode()
+  {
+    z = MAX_TOEGESTAAN + 4;
+    System.out.print("H");
+  }
+  public void mijnMethode(char j)
+  {
+    // MAX_TOEGESTAAN += 3; /* 2*/
+    mijnMethode();
+    System.out.print(j);
+    System.out.print(z);
+  }
+  public int som()
+  {
+    return MAX_TOEGESTAAN + z;
+  }
+  public static void main(String[] args)
+  {
+    MijnKlasse mk = new MijnKlasse();
+    mk.mijnMethode('K');
+  }
+}
